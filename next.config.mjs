66e7abce106ae4d9f2562c4010407b34b62e076f
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-10-19 12:53:59 +0200

    disable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+module.exports = {
+    swcMinify: false,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        // Disable minimization
+        config.optimization.minimize = false;
 
-export default nextConfig; 
+        // Optionally, you can remove the Terser plugin entirely
+        config.optimization.minimizer = config.optimization.minimizer.filter(
+            minimizer => minimizer.constructor.name !== 'TerserPlugin'
+        );
+
+        return config;
+    },
+}
