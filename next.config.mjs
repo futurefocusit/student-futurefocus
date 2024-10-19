9ce08401628ec5a9fee0dfd8b2fee04802e7bf36
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-10-19 12:37:26 +0200

    enable

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.optimization.minimize = false;
-        }
-        return config;
-    },
-};
+const nextConfig = {};
 
-export default nextConfig;
+export default nextConfig; 
