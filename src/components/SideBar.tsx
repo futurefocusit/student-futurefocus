f38b436276fbd1a5cfdeefe7aef3e7058b78e3cb
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-10-26 14:37:02 +0200

    icons

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -152,7 +152,7 @@ const SideBar = () => {
           <div className="px-4">
             <a
               href="https://www.futurefocus.co.rw/admin"
-              className={`flex items-center px-4 py-2 mb-10 text-gray-300 transition-colors duration-300 transform rounded-md hover:bg-gray-700 hover:text-white `}
+              className={`flex items-center px-4 py-2 mb-10 text-gray-300 transition-colors duration-300 transform rounded-md hover:bg-gray-700 hover:text-white ${loggedUser&&loggedUser.isAdmin?'':'hidden'} `}
             >
               <FaWebAwesome className="w-5 h-5" />
               <p className={`mx-4 font-medium ${isExpanded ? "" : "hidden"}`}>
