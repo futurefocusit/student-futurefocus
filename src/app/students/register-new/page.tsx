26c7c7dd808adcbdc5ef2bb8b98b1c30f5ef6fd8
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-11-25 13:01:20 +0200

    g

--- src/app/students/register-new/page.tsx
+++ src/app/students/register-new/page.tsx
@@ -13,6 +13,7 @@ import { useAuth } from "@/context/AuthContext";
 const imageUrl = "/futurefocuslogo.png";
 
 interface Course {
+  _id:string
   title: string;
   shifts: {_id:string,name:string}[];
 }
@@ -269,7 +270,7 @@ const data:IInvoice = {
              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
            >
              {courses.map((course) => (
-               <option key={course.title} value={course.title}>
+               <option key={course.title} value={course._id}>
                  {course.title}
                </option>
              ))}
